c21da833c83744a9178ea4031fb5ceae4dad29b5
TeaEye <ilya.tomskih@gmail.com>
2022-01-24 17:07:30 +0700

    tried to fix problems but something went wrong

--- src/main/java/Sort.java
+++ src/main/java/Sort.java
@@ -29,50 +29,62 @@ public class Sort {
                 return false;
             }
 
-        int min = 0;
+        int min;
         int indexOfMin = -1;
         List<String> stringValues = new ArrayList<>();
-        int[] intValues, indexesToDelete = new int[stringValues.size()];
+        List<Integer> intValues = new ArrayList<>();
+        int[] indexesToDelete;
+        int tmp, iter = 0;
 
-        int tmp = 0;
         do {
-            Arrays.fill(indexesToDelete, -1);
-
-            for (Scanner scanner: scanners)
+            tmp = 0;
+            for (Scanner scanner: scanners) {
                 if (scanner.hasNext()) {
-                    if (indexOfMin == scanners.indexOf(scanner) || indexOfMin == -1) stringValues.add(scanner.nextLine());
-                } else {
-                    stringValues.add("No value" + tmp);
-                    ++tmp;
-                }
+                    if (indexOfMin == -1) stringValues.add(scanner.nextLine());
+                    else if (indexOfMin == scanners.indexOf(scanner) && indexOfMin == tmp)
+                        stringValues.set(indexOfMin, scanner.nextLine());
+                } else
+                    if(indexOfMin != -1) stringValues.set(tmp, "No value" + tmp);
+                    else stringValues.add("No value" + tmp);
+                ++tmp;
+            }
+            indexesToDelete = new int[scanners.size()];
 
-            tmp ^= tmp;
+            tmp = 0;
+            for (String stringValue : stringValues) {
+                if (stringValue.equals("No value" + tmp)) indexesToDelete[tmp] = 1; //iTD == 1 means: this value is not correct, delete this scanner and string
+                else indexesToDelete[tmp] = -1;                                     //iTD == -1 means: this value is good, don't corrupt it
+                ++tmp;
+            }
 
-            for (String stringValue : stringValues)
-                if (stringValue.equals("No value" + tmp)) {
-                    indexesToDelete[tmp] = 1;
-                    ++tmp;
-                }
             for (int i = 0; i < indexesToDelete.length; ++i)
                 if (indexesToDelete[i] == 1) {
                     scanners.remove(i);
                     stringValues.remove(i);
                 }
+
             if (stringValues.isEmpty()) break;
-            intValues = new int[stringValues.size()];
-            for (int i = 0; i < intValues.length; ++i) intValues[i] = Integer.parseInt(stringValues.get(i));
-            min = Arrays.stream(intValues).min().getAsInt();
-            int finalMin = min;
-            int[] finalIntValues = intValues;
-            indexOfMin = IntStream.range(0, intValues.length).filter(i -> finalMin == finalIntValues[i]).findFirst().getAsInt();
+
+            for (int i = 0; i < stringValues.size(); ++i)
+                if (indexOfMin != i) intValues.add(Integer.parseInt(stringValues.get(i)));
+                else intValues.set(i, Integer.parseInt(stringValues.get(i)));
+
+            min = Collections.min(intValues);
+
+            for (indexOfMin = 0; indexOfMin < intValues.size(); ++indexOfMin) if (intValues.get(indexOfMin) == min) break;
+
+            indexesToDelete[indexOfMin] = 2;    //iTD == 2 means: we have already written this value, get next string
+
             try {
-                System.out.println("tried to write " + min);
+                final String out = "iter " + iter + ": tried to write " + min;
+                System.out.println(out);
                 fileWriter.write(String.valueOf(min));
             } catch (IOException e) {
                 System.out.println("ERROR InputOutput: couldn't write in file" + destination);
                 break;
             }
 
+            ++iter;
         } while (!stringValues.isEmpty());
 
         try {
